4d6f0144207b10ca803201074e26edc449f9d93d
Naandalist <listiananda.apriliawan@gmail.com>
2025-05-31 20:59:30 +0700

    feat: add DetailInfoSkeleton and DetailNewsSkeleton to exports in index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,10 +2,14 @@ import NewsCard from './NewsCard';
 import NewsCardSkeleton from './NewsCardSkeleton';
 import DetailEstimasiKeberangkatan from './DetailEstimasiKeberangkatan';
 import DetailInfoJemaahHaji from './DetailInfoJemaahHaji';
+import DetailInfoSkeleton from './DetailInfoSkeleton';
+import DetailNewsSkeleton from './DetailNewsSkeleton';
 
 export {
   NewsCard,
   NewsCardSkeleton,
   DetailEstimasiKeberangkatan,
   DetailInfoJemaahHaji,
+  DetailInfoSkeleton,
+  DetailNewsSkeleton,
 };
